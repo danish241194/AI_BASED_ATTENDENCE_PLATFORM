7b18c0defdbb8b5f1ab8d1c528abed1441cfcff4
Danish Mukhtar <danish241194@gmail.com>
2020-04-12 03:21:34 +0530

    add new person for training

---
+++ code/FaceRecognitionALgo/add_persons.py
@@ -0,0 +1,43 @@
+from imutils import paths
+import face_recognition
+import argparse
+import pickle
+import cv2
+import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i","--dataset",required=True)
+ap.add_argument("-e","--encodings",required=True)#olds path
+ap.add_argument("-d","--detection_method",type=str,default="hog")#cnn
+args = vars(ap.parse_args())
+
+dataset_path = args["dataset"]
+encodings_path = args["encodings"]
+detection_method = args["detection_method"]
+
+imagePaths = list(paths.list_images(dataset_path))
+
+knownEncodings = []
+knownNames = []
+
+print("+ Findings encodings")
+
+for(i,imagePath) in enumerate(imagePaths):
+	print("\t- Processing image {}/{}".format(i+1,len(imagePaths)))
+	name = imagePath.split(os.path.sep)[-2]
+	image = face_recognition.load_image_file(imagePath)
+	boxes = face_recognition.face_locations(image,model=detection_method)
+	encodings = face_recognition.face_encodings(image,boxes)
+	for encoding in encodings :
+		knownNames.append(name)
+		knownEncodings.append(encoding)
+
+data = pickle.loads(open(encodings_path,"rb").read())
+data["encodings"].extend(knownEncodings)
+data["names"].extend(knownNames)
+print("+ New Encodings : ",len(knownEncodings))
+print("+ Total Encodings : ",len(data["encodings"]))
+print("+ Dumping encodings")
+f = open(encodings_path,"wb")
+f.write(pickle.dumps(data))
+f.close()
