bf03f4fd844b5d6eab162208ecb9da9e9059664c
Shubham Pokhriyal <shubham.pokhriyal@students.iiit.ac.in>
2020-04-19 17:20:10 +0530

    querymanager

--- code/ApplicationManager/app.py
+++ code/ApplicationManager/app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template
-from flask import Flask, flash, request, redirect, render_template, make_response
+from flask import Flask, flash, request, redirect, render_template, make_response, url_for
 import urllib.request
 import requests   
 import os
@@ -80,7 +80,7 @@ def registerUser():
         credentials[userid]=password
         return render_template("login.html")
 
-def retrieveStudents(str):
+def retrieveListFromString(str):
     students = []
     temp = ""
     for i in range(len(str)):
@@ -93,35 +93,27 @@ def retrieveStudents(str):
         students.append(temp)
     return students
 
+
+# def addCamera(id):
+
+# def addStudents(id):
+
+# def removeStudents(id):
+
+
 @app.route("/addcourse/<id>", methods=['POST'])
 def addCourse(id):
     print(id)
-    print(type(id))
     course = request.form['course']
     room = request.form['room']
     students = request.form['student_list']
-    # days = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
-    # coursedays = []
-    # for day in days:
-    #     if request.form[day] != "":
-    #         coursedays.append(day)
     coursedays = request.form.getlist("day")
     time = request.form['time']
     duration = request.form['duration']
-    # user = request.form['user']
     if not course or not room or not students or not time or not duration or len(coursedays)==0:
         flash("Missing fields")
-        return render_template("institutehome.html")
-    studentlist = retrieveStudents(students)
-    print(course)
-    print(room)
-    print(studentlist)
-    print(coursedays)
-    print(time)
-    print(duration)
-    print("-------")
-    inst_id = id
-    print(inst_id)
+        return render_template("institutehome.html",user = id)
+    studentlist = retrieveListFromString(students)
     data = {}
     data["course"]=course
     data["room_number"]=room
@@ -130,19 +122,66 @@ def addCourse(id):
     data["attendance_time"]=time
     data["attendance_duration"]=duration
     print(data)
-    picklepath = "../static/data/institutes/"
+    picklepath = "static/data/institutes/"
     picklepath = os.path.join(picklepath,id)
     if not path.exists(picklepath):
         os.mkdir(picklepath)
         picklepath = os.path.join(picklepath,"courses") 
         os.mkdir(picklepath)
+    else:
+        picklepath = os.path.join(picklepath,"courses") 
     
     picklepath += ("/"+course+".pickle")
-    
     pickle_out = open(picklepath,"wb")
     pickle.dump(data, pickle_out)
     pickle_out.close()
-    return render_template("institutehome.html")
+    flash("Course registered")
+    return render_template("institutehome.html",user = id)
+
+@app.route("/iqm/<id>")
+def renderQueryManager(id):
+    print(id)
+    return render_template("institutequery.html",user = id)
+
+
+def convertDate(date):
+    for i in range(len(date)):
+        if date[i]=='/':
+            date[i]='-'
+    return date
+
+@app.route("/iqm/institutequery/<id>",methods=['POST','GET'])
+def instituteQuery(id):
+    if not request.form :
+        return render_template("institutequery.html",user=id )
+    coursestring = request.form['courselist']
+    studentstring = request.form['studentlist']
+    startdate = request.form['start']
+    enddate = request.form['end']
+    criterion = request.form['criterion']
+    if not startdate or not enddate or not criterion or len(coursestring)==0 or len(studentstring)==0:
+        flash("incomplete")
+        return redirect(url_for("instituteQuery",id=id))
+        # return render_template("institutequery.html",user=id )
+    courses = retrieveListFromString(coursestring)
+    students = retrieveListFromString(studentstring)
+    query = {}
+    query["courses"] = courses
+    query["students"] = students
+    query["start_date"] = convertDate(startdate)
+    query["end_date"] = convertDate(enddate)
+    if criterion != 0:
+        temp = {}
+        temp["greater_than"] = criterion
+        query["condition"] = temp
+    else:
+        query["condition"] = None
+
+    request_data = {}
+    request_data["institute_id"]=id
+    request_data["query"] = query
+    print(request_data)
+    return render_template("institutequery.html",user=id )
 
 
 if __name__ == "__main__":        # on running python app.py
