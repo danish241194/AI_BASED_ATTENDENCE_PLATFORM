edf6f47526130c587bfaad01005b18b34ca15e04
Neelesh Bhakt <42548994+neele2018201022@users.noreply.github.com>
2020-04-22 00:23:21 +0530

    Initial Bootstraping Done

--- code/BootstrapManager/bootstrapmanager.py
+++ code/BootstrapManager/bootstrapmanager.py
@@ -6,40 +6,106 @@ import threading
 import paramiko
 
 
+def copy(ssh_client,ftp_client,source, target):
+		# print(source,target)
+		for item in os.listdir(source):
+				if os.path.isfile(os.path.join(source, item)):
+						# print("Enter")
+						# print("File=>",item)
+						
+						ftp_client.put(os.path.join(source, item),str(target)+"/"+str(item))
+						# print("Exit")
+				else:
+						# print("folder=>",item)
+						stdin,stdout,stderr=ssh_client.exec_command("mkdir "+str(target)+"/"+str(item))
+						copy(ssh_client,ftp_client,os.path.join(source, item), str(target)+"/"+str(item))
+
+
 def runRegistry():
-	subprocess.call("python3 Folder/Folder.py '-p 5000'", shell=True)
+	subprocess.call("python3 Folder/Folder.py '-p 5982'", shell=True)
 
 def getdummyData():
-	with open("dummyfreelist.json") as f:
+	with open("freelist.json") as f:
 			  content= json.load(f)
 	return content
+
+
+def Update(freelist,machine1,machine2):
+
+		del freelist['machines'][machine1]
+		del freelist['machines'][machine2]
+		with open('newfreelist.json', 'w') as data_file:
+							data = json.dump(freelist, data_file)
+
+		return data   	
 def startBootstrap():
-	# try:
-	# 	os.mkdir("Folder")
-	# except:
-	# 	#Directory exists or some error
-	# 	flag=False
 
-	# shutil.copy("../Registry/registry.py","Folder/registry.py")
-	# t1 = threading.Thread(target=runRegistry)
-	# t1.start()
-	# time.sleep(5)
-	#use this for real use
-	#res = requests.get('http://127.0.0.1:5000/get_free_list')
-	#res=res.json()
+	freelist=getdummyData()
+	machine_list=list(freelist["machines"].keys())
+	machine1=freelist["machines"][machine_list[0]]
+	machine2=freelist["machines"][machine_list[1]]
+	ssh_client1 =paramiko.SSHClient()
+	ssh_client1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	ssh_client2 =paramiko.SSHClient()
+	ssh_client2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	ssh_client1.connect(hostname=machine1["ip"],username=machine1["username"],password=machine1["password"])
+	ssh_client2.connect(hostname=machine2["ip"],username=machine2["username"],password=machine2["password"])
 
-	#For testing
-	res=getdummyData()
-	key=list(res["machines"].keys())
-	machine1=res["machines"][key[0]]
-	machine2=res["machines"][key[1]]
-	ssh_client =paramiko.SSHClient()
-	ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	print("enter")
-	ssh_client.connect(hostname='127.0.0.1',username='n',password='')
-	stdin,stdout,stderr=ssh_client.exec_command("ls")
-	print(stdout.readlines())
-	# subprocess.call(['Folder/Folder.py', '-p', '5000']) 
+	stdin,stdout,stderr=ssh_client1.exec_command("mkdir Project")
+	stdin,stdout,stderr=ssh_client1.exec_command("mkdir Course")
+	stdin,stdout,stderr=ssh_client1.exec_command("mkdir Images")
+	stdin,stdout,stderr=ssh_client1.exec_command("mkdir Encodings")
+	stdin,stdout,stderr=ssh_client2.exec_command("mkdir Project")
+
+
+	ftp_client1=ssh_client1.open_sftp()
+	copy(ssh_client1,ftp_client1,'../ApplicationManager','Project')
+	copy(ssh_client1,ftp_client1,'../DeploymentManager','Project')
+	copy(ssh_client1,ftp_client1,'../Registry','Project')
+	
+
+
+	ftp_client2=ssh_client2.open_sftp()
+	# copy(ssh_client2,ftp_client,'../ServiceLifeCycleManager','Project')
+	copy(ssh_client2,ftp_client2,'../ServerLifeCycle','Project')
+	# # stdin,stdout,stderr=ssh_client1.exec_command("cp  deploymentmanager.py")
+	# print("enter")
+
+	## ==> RUNNING MACHINE ONE
+	print("Running Registry in Machine=",machine1["username"])
+	stdin,stdout,stderr=ssh_client1.exec_command("python3 Project/registry.py -p 5982")
+
+	print("Running Deployment Manager in Machine=",machine1["username"])
+	stdin,stdout,stderr=ssh_client1.exec_command("python3 Project/run_deployment_manager.py")
+	# print(stderr.readlines())
+
+	print("Running Application Manager in Machine=",machine1["username"])
+	stdin,stdout,stderr=ssh_client1.exec_command("python3 Project/app.py")
+	# print(stderr.readlines())
+
+	## ==> RUNNING MACHINE TWO
+	print("Running Server Life Cycle Manager in Machine=",machine2["username"])
+	stdin,stdout,stderr=ssh_client2.exec_command("python3 Project/run_server_lcm.py")
+
+	print("Free list sent to ServiceLifeCycleManager")
+	freelist=Update(freelist,machine_list[0],machine_list[1])
+	ftp_client2.put("newfreelist.json","Project/freelist.json")
+	# print(stderr.readlines())
+	ftp_client1.close()
+	ftp_client2.close()
+
+
+	# stdin,stdout,stderr=ssh_client1.exec_command("ls")
+	# print(stdout.readlines())
+	# # print("hhshs")
+	# # res = requests.get('http://127.0.0.1:5982/get_free_list')
+	# # print(res.json())
+	# # stdin,stdout,stderr=ssh_client1.exec_command("python3 deploymentmanager.py")
+	# stdin,stdout,stderr=ssh_client1.exec_command("python3 -m pip install --user flask")
+	# stdin,stdout,stderr=ssh_client1.exec_command("python3 -m pip install --user flask_cors")
+	# stdin,stdout,stderr=ssh_client1.exec_command("python3 app.py")
+	# print(stderr.readlines())
+	# subprocess.call(['Folder/Folder.py', '-p', '5982']) 
 	# exec()
 	# python3 "Folder/Folder.py"
 
@@ -51,7 +117,7 @@ def startBootstrap():
 	images
 	encodings
 
-	2.Copy the code of registry into Project FOLDER and run this registry code py passing port number 5000
+	2.Copy the code of registry into Project FOLDER and run this registry code py passing port number 5982
 	which is fix in our platform
 	2.read the free list file which will be in the following format
 		machines:[
